5ede73f465981a7cd97f8bdc6d47d2da1ef948d2
Kai Zhang <kaizhang@tscc-login2.sdsc.edu>
2022-02-17 10:14:55 -0800

    update genome coverage

--- src/bed/tree.rs
+++ src/bed/tree.rs
@@ -1,12 +1,12 @@
 use std::ops::Range;
 use bio::data_structures::interval_tree::*;
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 use itertools::Itertools;
 use std::hash::Hash;
 use num::Integer;
 use num_traits::{Num, NumAssignOps};
 
-use super::{GenomicRange, BEDLike};
+use super::{GenomicRange, BEDLike, split_by_len};
 
 pub struct BedTree<D>(HashMap<String, IntervalTree<u64, D>>);
 
@@ -75,52 +75,10 @@ pub struct GenomeRegions<B> {
     pub indices: BedTree<usize>,
 }
 
-// TODO: add tests.
 impl<B: BEDLike> GenomeRegions<B> {
     pub fn get_regions(&self) -> &Vec<B> { &self.regions }
 
     pub fn len(&self) -> usize { self.regions.len() }
-
-    /// Calculate coverage on a set of (not necessarily unique) genomic regions.
-    /// Regions would be allocated.
-    pub fn get_coverage<R, I, N>(&self, tags: R) -> (Vec<N>, u64)
-    where
-        R: Iterator<Item = I>,
-        I: BEDLike,
-        N: Num + NumAssignOps + Copy,
-    {
-        let mut coverage = vec![N::zero(); self.regions.len()];
-        let mut num_tag = 0;
-        for tag in tags {
-            num_tag += 1;
-            self.indices.find(&tag).for_each(|(_, idx)| coverage[*idx] += N::one());
-        }
-        (coverage, num_tag)
-    }
-
-    pub fn get_binned_coverage<R, I, N>(&self,
-                                         bin_size: u64,
-                                         tags: R) -> (Vec<Vec<N>>, u64)
-    where
-        R: Iterator<Item = I>,
-        I: BEDLike,
-        N: Num + NumAssignOps + Copy,
-    {
-        let mut coverage: Vec<Vec<N>> = self.regions.iter()
-            .map(|x| vec![N::zero(); x.len().div_ceil(&bin_size) as usize]).collect();
-        let mut num_tag = 0;
-        for tag in tags {
-            num_tag += 1;
-            self.indices.find(&tag)
-                .for_each(|(region, out_idx)| {
-                    let i = tag.start().saturating_sub(region.start()).div_floor(&bin_size);
-                    let j = (tag.end() - 1 - region.start())
-                        .min(region.len() - 1).div_floor(&bin_size);
-                    (i..=j).for_each(|in_idx| coverage[*out_idx][in_idx as usize] += N::one());
-                });
-        }
-        (coverage, num_tag)
-    }
 }
 
 impl<B: BEDLike> FromIterator<B> for GenomeRegions<B> {
@@ -131,12 +89,98 @@ impl<B: BEDLike> FromIterator<B> for GenomeRegions<B> {
     }
 }
 
-pub struct BinnedCoverage<'a, B, N> {
-    genome_regions: &'a GenomeRegions<B>,
-    pub size: usize,
-    pub bin_size: u64,
-    coverage: Vec<Vec<N>>,
+pub struct Coverage<'a, B, N> {
     pub consumed_tags: u64,
+    genome_regions: &'a GenomeRegions<B>,
+    coverage: Vec<N>,
+}
+
+impl <'a, N: Num + NumAssignOps + Copy, B: BEDLike> Coverage<'a, B, N> {
+    pub fn new(genome_regions: &'a GenomeRegions<B>) -> Self {
+        Self {
+            consumed_tags: 0,
+            genome_regions,
+            coverage: vec![N::zero(); genome_regions.len()],
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.consumed_tags = 0;
+        self.coverage.fill(N::zero());
+    }
+
+    pub fn add<D>(&mut self, tag: &D)
+    where
+        D: BEDLike,
+    {
+        self.consumed_tags += 1;
+        self.genome_regions.indices.find(tag).for_each(|(_, idx)| self.coverage[*idx] += N::one());
+    }
+
+    pub fn get_regions(&'a self) -> impl Iterator<Item = &B> + 'a
+    where
+        B: Clone
+    {
+        self.genome_regions.regions.iter()
+    }
+
+    pub fn get_coverage(&self) -> &Vec<N> { &self.coverage }
+}
+
+
+pub struct SparseCoverage<'a, B, N> {
+    pub consumed_tags: u64,
+    genome_regions: &'a GenomeRegions<B>,
+    coverage: BTreeMap<usize, N>,
+}
+
+impl <'a, N: Num + NumAssignOps + Copy, B: BEDLike> SparseCoverage<'a, B, N> {
+    pub fn new(genome_regions: &'a GenomeRegions<B>) -> Self {
+        Self {
+            consumed_tags: 0,
+            genome_regions,
+            coverage: BTreeMap::new(),
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.consumed_tags = 0;
+        self.coverage = BTreeMap::new();
+    }
+
+    pub fn add<D>(&mut self, tag: &D)
+    where
+        D: BEDLike,
+    {
+        self.consumed_tags += 1;
+        self.genome_regions.indices.find(tag).for_each(|(_, idx)| {
+            let counter = self.coverage.entry(*idx).or_insert(N::zero());
+            *counter += N::one();
+        });
+    }
+
+    pub fn get_regions(&'a self) -> impl Iterator<Item = &B> + 'a
+    where
+        B: Clone
+    {
+        self.genome_regions.regions.iter()
+    }
+
+    pub fn get_coverage(&self) -> &BTreeMap<usize, N> { &self.coverage }
+
+    pub fn get_coverage_as_vec(&self) -> Vec<N> {
+        let mut coverage = vec![N::zero(); self.genome_regions.len()];
+        self.coverage.iter().for_each(|(idx, v)| coverage[*idx] = *v);
+        coverage
+    }
+}
+
+pub struct BinnedCoverage<'a, B, N> {
+    pub len: usize,
+    pub bin_size: u64,
+    pub consumed_tags: u64,
+    genome_regions: &'a GenomeRegions<B>,
+    coverage: Vec<Vec<N>>,
 }
 
 impl <'a, N: Num + NumAssignOps + Copy, B: BEDLike> BinnedCoverage<'a, B, N> {
@@ -144,8 +188,8 @@ impl <'a, N: Num + NumAssignOps + Copy, B: BEDLike> BinnedCoverage<'a, B, N> {
         let coverage: Vec<Vec<N>> = genome_regions.regions.iter()
             .map(|x| vec![N::zero(); x.len().div_ceil(&bin_size) as usize])
             .collect();
-        let size = coverage.iter().map(|x| x.len()).sum();
-        Self {genome_regions, size, bin_size, coverage, consumed_tags: 0}
+        let len = coverage.iter().map(|x| x.len()).sum();
+        Self {genome_regions, len, bin_size, coverage, consumed_tags: 0}
     }
 
     pub fn reset(&mut self) {
@@ -177,6 +221,69 @@ impl <'a, N: Num + NumAssignOps + Copy, B: BEDLike> BinnedCoverage<'a, B, N> {
     pub fn get_coverage(&self) -> &Vec<Vec<N>> { &self.coverage }
 }
 
+pub struct SparseBinnedCoverage<'a, B, N> {
+    pub len: usize,
+    pub bin_size: u64,
+    pub consumed_tags: u64,
+    genome_regions: &'a GenomeRegions<B>,
+    accu_size: Vec<usize>,
+    coverage: BTreeMap<usize, N>,
+}
+
+impl <'a, N: Num + NumAssignOps + Copy, B: BEDLike> SparseBinnedCoverage<'a, B, N> {
+    pub fn new(genome_regions: &'a GenomeRegions<B>, bin_size: u64) -> Self {
+        let mut len = 0;
+        let accu_size = genome_regions.regions.iter().map(|x| {
+            let n = x.len().div_ceil(&bin_size) as usize;
+            let output = len;
+            len += n;
+            output
+        }).collect();
+        Self {
+            len, bin_size, consumed_tags: 0, genome_regions, accu_size,
+            coverage: BTreeMap::new()
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.consumed_tags = 0;
+        self.coverage = BTreeMap::new();
+    }
+
+    pub fn add<D>(&mut self, tag: &D)
+    where
+        D: BEDLike,
+    {
+        self.consumed_tags += 1;
+        self.genome_regions.indices.find(tag).for_each(|(region, out_idx)| {
+            let i = tag.start().saturating_sub(region.start()).div_floor(&self.bin_size);
+            let j = (tag.end() - 1 - region.start())
+                .min(region.len() - 1).div_floor(&self.bin_size);
+            let n = self.accu_size[*out_idx];
+            (i..=j).for_each(|in_idx| {
+                let counter = self.coverage.entry(n + in_idx as usize).or_insert(N::zero());
+                *counter += N::one();
+            });
+        });
+    }
+
+    pub fn get_regions(&'a self) -> impl Iterator<Item = impl Iterator<Item = B>> + 'a
+    where
+        B: Clone
+    {
+        self.genome_regions.regions.iter()
+            .map(|x| split_by_len(x, self.bin_size))
+    }
+
+    pub fn get_coverage(&self) -> &BTreeMap<usize, N> { &self.coverage }
+
+    pub fn get_coverage_as_vec(&self) -> Vec<N> {
+        let mut coverage = vec![N::zero(); self.len];
+        self.coverage.iter().for_each(|(idx, v)| coverage[*idx] = *v);
+        coverage
+    }
+}
+
 #[cfg(test)]
 mod bed_intersect_tests {
     use super::*;
@@ -197,5 +304,75 @@ mod bed_intersect_tests {
         let expected = vec![GenomicRange::new("chr1".to_string(), 100, 210)];
         assert_eq!(result, expected);
     }
+
+    #[test]
+    fn test_coverage() {
+        let bed_set1: Vec<GenomicRange> = vec![
+            GenomicRange::new("chr1".to_string(), 200, 500),
+            GenomicRange::new("chr1".to_string(), 1000, 2000),
+            GenomicRange::new("chr1".to_string(), 10000, 11000),
+            GenomicRange::new("chr10".to_string(), 10, 20),
+        ];
+        let bed_set2: Vec<GenomicRange> = vec![
+            GenomicRange::new("chr1".to_string(), 100, 210),
+            GenomicRange::new("chr1".to_string(), 100, 500),
+            GenomicRange::new("chr1".to_string(), 100, 5000),
+            GenomicRange::new("chr1".to_string(), 100, 200),
+            GenomicRange::new("chr1".to_string(), 1000, 1001),
+        ];
+
+        let regions: GenomeRegions<GenomicRange> = bed_set1.into_iter().collect();
+
+        let mut cov1 = Coverage::new(&regions);
+        bed_set2.iter().for_each(|x| cov1.add(x));
+        let result1: Vec<u64> = cov1.get_coverage().to_vec();
+        let mut cov2 = SparseCoverage::new(&regions);
+        bed_set2.iter().for_each(|x| cov2.add(x));
+        let result2: Vec<u64> = cov2.get_coverage_as_vec();
+
+        assert_eq!(result1, result2);
+        assert_eq!(result1, vec![3, 2, 0, 0]);
+
+        let mut cov3 = BinnedCoverage::new(&regions, 100);
+        bed_set2.iter().for_each(|x| cov3.add(x));
+        let result3: Vec<u64> = cov3.get_coverage().iter().flatten().map(|x| *x).collect();
+        let mut cov4 = SparseBinnedCoverage::new(&regions, 100);
+        bed_set2.iter().for_each(|x| cov4.add(x));
+        let result4: Vec<u64> = cov4.get_coverage_as_vec();
+
+        assert_eq!(result3, result4);
+        assert_eq!(
+            result3,
+            vec![
+                3, 2, 2,
+                2, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_sparse_coverage() {
+        let regions = [
+            GenomicRange::new("chr1", 0, 2000),
+            GenomicRange::new("chr2", 100, 2100),
+            GenomicRange::new("chr3", 3000, 3500),
+            ].into_iter().collect();
+        let mut cov = SparseBinnedCoverage::new(&regions, 400);
+
+        cov.add(&GenomicRange::new("chr1", 3, 5));
+        cov.add(&GenomicRange::new("chr2", 0, 500));
+        cov.add(&GenomicRange::new("chr2", 0, 501));
+        cov.add(&GenomicRange::new("chr3", 3400, 3401));
+        cov.add(&GenomicRange::new("chr4", 0, 501));
+        cov.add(&GenomicRange::new("chr3", 0, 501));
+
+        assert_eq!(
+            vec![(0, 1), (5, 2), (6, 1), (11, 1)],
+            cov.get_coverage().iter().map(|(i, x)| (*i, *x)).collect::<Vec<(usize, u64)>>()
+        );
+    }
+
 }
 
